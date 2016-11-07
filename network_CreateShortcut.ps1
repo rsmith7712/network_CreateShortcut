931c314b6c22fa2ceca2cf53f88825e8838f2780
rsmith7712 <richard.smith7712@gmail.com>
2016-11-07 15:07:04 -0800

    Add files via upload

---
+++ network_CreateShortcut.ps1
@@ -0,0 +1,18 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.127
+	 Created on:   	7/22/2016 9:40 AM
+	 Created by:   	Richard Smith
+	 Organization: 	
+	 Filename:     	network_CreateShortcut.ps1
+	===========================================================================
+	.DESCRIPTION
+		Create desktop shortcut with PowerShell
+#>
+
+$Shell = New-Object -ComObject WScript.Shell
+$Shortcut = $Shell.CreateShortcut("$Env:Public\Desktop\Google.com.lnk")
+$Shortcut.TargetPath = "https://www.google.com"
+$Shortcut.IconLocation = "shell32.dll,43"
+$Shortcut.Save()
